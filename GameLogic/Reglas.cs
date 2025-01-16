3fabcb0c331f35bcea50657f490c633d5bdf1c11
Kikiale <kikialeglez@gmail.com>
2025-01-16 17:40:27 -0500

    Anadimos una clase reglas de pura programacion funcional con el objetivo de ir cambiando las reglas a medio juego con las habilidades de los jugadores

---
+++ GameLogic/Reglas.cs
@@ -0,0 +1,6 @@
+public static class Reglas 
+{
+    public static Action<IFicha> Move;
+    public static Func<int> NextTurn;
+    public static Action<IModificador> TrapSistem;
+}
--- obj/Debug/net8.0/MazeRunner.AssemblyInfo.cs
+++ obj/Debug/net8.0/MazeRunner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MazeRunner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78fb3a28df83c1791fffe703d44144ddc53047f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277ee29a75aaad800683efe656950ba4277fe9c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MazeRunner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MazeRunner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
